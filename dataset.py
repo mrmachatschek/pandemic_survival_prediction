bff8888ed50ebdd76e4c8aea8b9bd68be52031bd
Alter93 <alexae93@gmail.com>
2020-05-10 21:43:25 -0500

    SVM Ready

--- dataset.py
+++ dataset.py
@@ -1,12 +1,40 @@
 import pandas as pd
 import numpy as np
 
+'''
+'Family_Case_ID',
+'Severity',
+'Birthday_year',
+'Parents or siblings infected',
+'Wife/Husband or children infected',
+'Medical_Expenses_Family',
+'Medical_Tent_A',
+'Medical_Tent_B',
+'Medical_Tent_C',
+'Medical_Tent_D',
+'Medical_Tent_E',
+'Medical_Tent_F',
+'Medical_Tent_G',
+'Medical_Tent_T',
+'Medical_Tent_n/a',
+'City_Albuquerque',
+'City_Santa Fe',
+'City_Taos',
+'Gender_M',
+'family_size'
+'''
+
+# Severity Against Age
+# Medical Expenses Against Severity
+
+# Sibling Infected / Family Size
+# Severity Against Severity in Medical Tent
+# Severity Against Severity in City
+
 class Dataset():
     def __init__(self):
         self.train_data = pd.read_csv('train.csv', index_col="Patient_ID")
         self.test_data = pd.read_csv('test.csv', index_col="Patient_ID")
-
-
 
         self.train_data = Dataset.prepare_data(self.train_data)
         self.test_data = Dataset.prepare_data(self.test_data)
@@ -19,11 +47,15 @@ class Dataset():
         del self.train_data['Deceased']
 
 
-
-
     def create_features(data):
         df_encoded = data.copy()
         df_encoded["family_size"] = [df_encoded.Family_Case_ID.value_counts()[fid] for fid in df_encoded.Family_Case_ID.values]
+        df_encoded["spending_vs_severity"] = df_encoded["Medical_Expenses_Family"] / df_encoded["Severity"]
+        df_encoded["spending_family_member"] = df_encoded["Medical_Expenses_Family"] / (df_encoded['Parents or siblings infected'] + df_encoded['Wife/Husband or children infected'] + 1)
+        df_encoded["severity_against_avg_city"] = df_encoded["Severity"] / df_encoded["Sev_by_city"]
+        df_encoded["severity_against_avg_tent"] = df_encoded["Severity"] / df_encoded["Sev_by_tent"]
+        df_encoded["severity_against_avg_gender"] = df_encoded["Severity"] / df_encoded["Sev_by_gender"]
+        df_encoded["spending_family_severity"] = ( df_encoded["Medical_Expenses_Family"] / (df_encoded['Parents or siblings infected'] + df_encoded['Wife/Husband or children infected'] + 1) ) / df_encoded["Severity"]
 
         return df_encoded
 
@@ -34,14 +66,33 @@ class Dataset():
                   'Master':"M",
                   'Miss':"F",
                   "Mrs.":"F"}
+        df['Medical_Tent'] = df['Medical_Tent'].replace(np.nan, 'n/a', regex=True)
+        df['Birthday_year'] = df['Birthday_year'].replace(np.nan, -1, regex=True)
 
         df['Name'] = df['Name'].str.rsplit(' ', n=0, expand=True)
         df['Gender'] = [gender[item] for item in df['Name']]
 
         del df['Name']
 
-        df['Medical_Tent'] = df['Medical_Tent'].replace(np.nan, 'n/a', regex=True)
-        df['Birthday_year'] = df['Birthday_year'].replace(np.nan, -1, regex=True)
+        mean = df[["Severity",'City']].groupby(['City']).mean()
+        mean["Sev_by_city"] = mean["Severity"]
+        del mean["Severity"]
+        df = df.join(mean["Sev_by_city"], on='City', how='left')
+
+        mean = df[["Severity",'Medical_Tent']].groupby(['Medical_Tent']).mean()
+        mean["Sev_by_tent"] = mean["Severity"]
+        del mean["Severity"]
+        df = df.join(mean["Sev_by_tent"], on='Medical_Tent', how='left')
+
+        mean = df[["Severity",'Gender']].groupby(['Gender']).mean()
+        mean["Sev_by_gender"] = mean["Severity"]
+        del mean["Severity"]
+        df = df.join(mean["Sev_by_gender"], on='Gender', how='left')
+
+        mean = df[["Severity",'Family_Case_ID']].groupby(['Family_Case_ID']).mean()
+        mean["Sev_family"] = mean["Severity"]
+        del mean["Severity"]
+        df = df.join(mean["Sev_family"], on='Family_Case_ID', how='left')
 
         df = df[df['City'].notna()]
         df = pd.get_dummies(df, prefix=['Medical_Tent', 'City', 'Gender'])
--- svm.py
+++ svm.py
@@ -4,55 +4,160 @@ from sklearn import svm
 import matplotlib.pyplot as plt
 from sklearn import tree
 from dataset import Dataset
-from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
 from sklearn.model_selection import KFold
 
 class ModelSVM():
-    def __init__(self):
-        self.dataset = Dataset()
-        self.model = svm.SVC(C=1.0,
-                             kernel='rbf',
-                             degree=3,
-                             gamma='scale',
-                             coef0=0.0,
-                             shrinking=True,
-                             probability=False,
-                             tol=0.001,
-                             cache_size=200,
-                             class_weight=None,
-                             verbose=False,
-                             max_iter=-1,
-                             decision_function_shape='ovr',
-                             break_ties=False,
-                             random_state=0)
-                             
-        k_fold = KFold(n_splits=5)
+    def __init__(self, vector_model = svm.NuSVC):
+        dataset = Dataset()
 
+        print(f"Model SVM - {vector_model}")
+        self.model = vector_model()
+
+        self.train_data = dataset.train_data
+        self.test_data = dataset.test_data
+        self.target = dataset.target
+
+        self.selected_variables = [
+            'Severity',
+            'Gender_M',
+            'City_Albuquerque',
+            'City_Santa Fe',
+            "severity_against_avg_gender",
+            'Medical_Tent_n/a',
+            "Sev_family",
+            'spending_family_member',
+            'family_size',
+
+            # 'Birthday_year',
+            # 'Parents or siblings infected',
+            # 'Wife/Husband or children infected',
+            # 'Medical_Tent_A',
+            # 'Medical_Tent_B',
+            # 'Medical_Tent_C',
+            # 'Medical_Tent_D',
+            # 'Medical_Tent_E',
+            # 'Medical_Tent_F',
+            # 'Medical_Tent_G',
+            # 'Medical_Tent_T',
+            # 'Medical_Expenses_Family',
+            # 'City_Taos',
+            # 'spending_vs_severity',
+            # "severity_against_avg_city",
+            # "severity_against_avg_tent",
+            # "spending_family_severity",
+
+
+            ]
+
+        scaler = RobustScaler().fit(self.test_data[self.selected_variables])
+
+        self.train_data[self.selected_variables] = scaler.transform(
+                               self.train_data[self.selected_variables])
+
+        self.test_data[self.selected_variables] = scaler.transform(
+                                self.test_data[self.selected_variables])
+
+        self.test_data = self.test_data[self.selected_variables]
+        self.train_data = self.train_data[self.selected_variables]
+
+    def run_model(self,path = None):
+        self.create_kfolds()
+        self.train_model()
+        self.predict_results(path)
+
+    def draw_boxplots(self):
+        train_data = self.train_data.join(self.target,how='left')
+        boxplot_1 = train_data.boxplot(column=[
+                                               'Severity',
+                                               'Birthday_year',
+                                               'Parents or siblings infected',
+                                               'Wife/Husband or children infected',
+                                               ],
+                                       by=["Deceased"])
+
+        boxplot_2 = train_data.boxplot(column=[
+                                                'Medical_Expenses_Family',
+                                                'City_Albuquerque',
+                                                'City_Santa Fe',
+                                                'City_Taos',
+                                               ],
+                                       by=["Deceased"])
+
+        boxplot_3 = train_data.boxplot(column=[
+                                                'Gender_M',
+                                                'family_size',
+                                                'spending_vs_severity',
+                                                'spending_family_member',
+                                              ],
+                                       by=["Deceased"])
+
+        boxplot_4 = train_data.boxplot(column=[
+                                                "severity_against_avg_city",
+                                                "severity_against_avg_tent",
+                                                "Sev_family",
+                                                "spending_family_severity",
+                                              ],
+                                       by=["Deceased"])
+        boxplot_5 = train_data.boxplot(column=['Medical_Tent_G','Medical_Tent_T','Medical_Tent_n/a',"severity_against_avg_gender"],by=["Deceased"])
+
+        boxplot_6 = train_data.boxplot(column=[
+                                               'Medical_Tent_A',
+                                               'Medical_Tent_B',
+                                               'Medical_Tent_C',
+                                               'Medical_Tent_D',
+                                               'Medical_Tent_E',
+                                               'Medical_Tent_F',
+                                             ],
+                                      by=["Deceased"])
+        plt.show()
+
+
+    def create_kfolds(self):
+        k_fold = KFold(n_splits=5)
         self.y_train = []
         self.x_train = []
-
         self.y_test = []
         self.x_test = []
 
-        for train_index, test_index in k_fold.split(self.dataset.train_data):
-            X_train, X_test = self.dataset.train_data.iloc[train_index], self.dataset.train_data.iloc[test_index]
-            Y_train, Y_test = self.dataset.target.iloc[train_index], self.dataset.target.iloc[test_index]
+        for train_index, test_index in k_fold.split(self.train_data):
+            X_train = self.train_data.iloc[train_index]
+            X_test = self.train_data.iloc[test_index]
+            Y_train = self.target.iloc[train_index]
+            Y_test = self.target.iloc[test_index]
             self.x_train.append(X_train)
             self.x_test.append(X_test)
             self.y_train.append(Y_train)
             self.y_test.append(Y_test)
 
     def train_model(self):
+        average_accuracy = 0
+        best_accuracy = 0
+        best_model = None
         for index in range(5):
             self.model.fit(self.x_train[index],self.y_train[index])
-            print(self.model.score(self.x_test[index], self.y_test[index]))
+            score = self.model.score(self.x_test[index], self.y_test[index])
+            if(score > best_accuracy):
+                best_accuracy = score
+                best_model = index
 
+            average_accuracy = average_accuracy + score
 
-    def predict_results(self,path="solution.csv"):
-        labels_val = self.model.predict(self.dataset.test_data)
-        self.dataset.test_data.insert(loc=0, column='Deceased', value=labels_val)
-        self.dataset.test_data["Deceased"].to_csv(path)
+        average_accuracy = average_accuracy / 5
 
-model = ModelSVM()
-model.train_model()
-model.predict_results()
+        print("Average model accuracy: {:2.2%}".format(average_accuracy))
+        print("Highest model accuracy: {:2.2%}".format(best_accuracy))
+        self.model.fit(self.x_train[best_model],self.y_train[best_model])
+
+    def predict_results(self,path = None):
+        labels_val = self.model.predict(self.test_data)
+        self.test_data.insert(loc=0, column='Deceased', value=labels_val)
+        if (path != None):
+            self.test_data["Deceased"].to_csv(path)
+            print(f"Solution set saved as '{path}'.")
+        else:
+            print(f"Solution not saved.")
+            self.test_data["Deceased"]
+
+# model = ModelSVM(vector_model = svm.NuSVC)
+# model.run_model(path="solution.csv")
